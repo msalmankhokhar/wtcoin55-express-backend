69d1db8a78ee9e551e88ed1793ecdc633a569780
Myles181 <cyrile450@gmail.com>
2025-06-24 02:57:30 +0100

    222

--- models/future-order.js
+++ models/future-order.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const FuturesOrderHistorySchema = new mongoose.Schema({
-    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    user: { type: mongoose.Schema.Types.ObjectId, ref: 'Users', required: true },
     symbol: { type: String, required: true }, // ETHUSDT, BTCUSDT
     
     // Order Details
